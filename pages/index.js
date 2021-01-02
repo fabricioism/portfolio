517c18afb92cd721afd6f0ed19c8652e96fd94d0
Fabricio Murillo <fabriciomurillo24@gmail.com>
2021-01-02 00:43:47 -0600

    :art: Just messing around

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <Container>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          Hey, I’m Lee Robinson
+          Hello everybody! I'm Fabricio Ismael Murillo.
         </h1>
         <h2 className="text-gray-600 dark:text-gray-400 mb-16">
           I’m a developer, writer, and creator. I work at ▲Vercel as a Solutions
