9b257fbc45481258554ec3c6b27e00644b98df6e
Fabricio Ismael Murillo <fabriciomurillo24@gmail.com>
2021-05-12 17:26:30 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -32,6 +32,21 @@ export default function Footer() {
             </g>
           </svg>
         </ExternalLink>
+        <ExternalLink href="https://instagram.com/fabricioism">
+          <span className="sr-only">Instagram</span>
+          <svg className="h-5 w-5" viewBox="0 0 24 24">
+            <g
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </g>
+          </svg>
+        </ExternalLink>
         <ExternalLink href="https://github.com/fabricioism">
           <span className="sr-only">Github</span>
           <svg className="h-5 w-5" viewBox="0 0 24 24">
